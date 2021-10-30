7620f5ed6f386e61a5fc86af5ef348359ed56df4
maayankestler <maayan3002@gmail.com>
2021-10-31 01:16:22 +0300

    add meetings filternig on maps

--- LetsMeet/Views/MeetingsMapPage.xaml.cs
+++ LetsMeet/Views/MeetingsMapPage.xaml.cs
@@ -5,18 +5,18 @@ using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
-
+using System.Collections.ObjectModel;
+using System.Collections;
+using System.Threading;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using Xamarin.Forms.Maps;
+using Xamarin.Essentials;
+
+using LetsMeet.Controls;
 using LetsMeet.Data;
 using LetsMeet.ViewModels;
 using LetsMeet.Models;
-using System.Collections.ObjectModel;
-using System.Collections;
-using Xamarin.Essentials;
-using System.Threading;
-using LetsMeet.Controls;
 
 namespace LetsMeet.Views
 {
@@ -26,11 +26,48 @@ namespace LetsMeet.Views
         //public List<Meeting> AvailableMeetings { get; set; }
         public ObservableCollection<Meeting> _meetingsList { get; set; } = new ObservableCollection<Meeting>(MeetingsData.Meetings);
         public IEnumerable AvailableMeetings => _meetingsList;
+        private bool _onwedByMe = false;
+        public bool OwnedByMe { 
+            get
+            {
+                return _onwedByMe;
+            } 
+            set
+            {
+                _onwedByMe = value;
+                FilterMeetings();
+            } 
+        }
+        private bool _onwedByFriend = false;
+        public bool OwnedByFriend
+        {
+            get
+            {
+                return _onwedByFriend;
+            }
+            set
+            {
+                _onwedByFriend = value;
+                FilterMeetings();
+            }
+        }
+        private bool _member = false;
+        public bool Member
+        {
+            get
+            {
+                return _member;
+            }
+            set
+            {
+                _member = value;
+                FilterMeetings();
+            }
+        }
         public MeetingsMapPage()
         {
             InitializeComponent();
-            // _meetingsList = MeetingsData.Meetings; // TODO handle filters
-            //BindingContext = new MeetingMapViewModel();
+            FilterMeetings(); // TODO handle filters
             BindingContext = this;
         }
         async private void Pin_InfoWindowClicked(object sender, PinClickedEventArgs e)
@@ -47,6 +84,34 @@ namespace LetsMeet.Views
             Position CurrentPosition = new Position(location.Latitude, location.Longitude);
             ChooseMapControl.MoveToRegion(MapSpan.FromCenterAndRadius(CurrentPosition, Distance.FromMiles(1)));
             base.OnAppearing();
+        }
+
+        private void FilterMeetings()
+        {
+            ObservableCollection<Meeting> temp_meetings_list = new ObservableCollection<Meeting>();
+
+            void VerifyAdd(Meeting m)
+            {
+                if (!temp_meetings_list.Contains(m))
+                    temp_meetings_list.Add(m);
+            };
+
+            if (OwnedByMe)
+                MeetingsData.Meetings.FindAll(m => m.Owner.Id == MainViewModel.GetInstance.CurrentUser.Id).ForEach(VerifyAdd);
+            if (OwnedByFriend)
+                MeetingsData.Meetings.FindAll(m => MainViewModel.GetInstance.CurrentUser.Friends.Contains(m.Owner)).ForEach(VerifyAdd);
+            if (Member)
+                MeetingsData.Meetings.FindAll(m => m.Members.Contains(MainViewModel.GetInstance.CurrentUser)).ForEach(VerifyAdd);
+            if (!OwnedByMe && !OwnedByFriend && !Member)
+                temp_meetings_list = new ObservableCollection<Meeting>(MeetingsData.Meetings);
+            //MeetingsData.Meetings.FindAll(m => m.Members.Contains(MainViewModel.GetInstance.CurrentUser)).ForEach(
+            //    i => {
+            //        if (!temp_meetings_list.Contains(i))
+            //            temp_meetings_list.Add(i);
+            //    });
+            //else
+            //    temp_meetings_list = new ObservableCollection<Meeting>(MeetingsData.Meetings);
+            _meetingsList = temp_meetings_list;
         }
     }
 }
